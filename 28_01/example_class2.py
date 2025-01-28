0daad6e6de4d19f37168d7eeb1697aa4cfd7da77
anurag-balki03 <balkianurag04@example.com>
2025-01-28 14:49:30 +0530

    this is my second commit on day 5

--- 28_01/example_class2.py
+++ 28_01/example_class2.py
@@ -2,12 +2,23 @@ class employee:
     def __init__(self,name,salary):
         self._name=name
         self._salary=salary
-        self.allowances=int(input("enter the allowances:"))
+        
+    def allowances(self):
+        self.allowance1=int(input("enter the allowances for food:"))
+        self.allowance2=int(input("enter the allowances for stay:"))
+        self.allowance3=int(input("enter the allowances for travel:"))
+        return self.allowance1+self.allowance2+self.allowance3
+    
+    def deduction(self,salary):
+        self.deduct=int(3/100)*salary
+        print(f"tax deducted from salary: {self.deduct}")
+        return self.deduct
+        
     def set_salary(self,salary):
         self._salary=salary
     
     def get_salary(self):
-        return self._salary+self.allowances
+        return self._salary+self.allowances()-self.deduction(self._salary)
 
     def get_name(self):
         return self._name
